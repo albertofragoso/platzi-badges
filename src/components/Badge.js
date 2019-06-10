3e11474b36ddc04184f9f88e24d8d656843f7daf
albertofragoso <hola@albertofragoso.com>
2019-06-10 17:04:49 -0500

    Agregando props a Badge component.

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react'
 import './styles/Badge.css'
 
 import confLogo from '../images/badge-header.svg'
-
 class Badge extends Component {
+  
   render() {
+    const { firstName, lastName, avatarUrl, jobTitle, website } = this.props
+
     return <div className="Badge">
       <div className="Badge__header">
         <img src={confLogo} alt="Logo de la conferencia" />
       </div>
       <div className="Badge__section-name">
-        <img className="Badge__avatar" src="https://avatars0.githubusercontent.com/u/24305046?s=460&v=4" alt="Usuario de la conferencia" />
-        <h1>Alberto <br /> Fragoso</h1>
+        <img className="Badge__avatar" src={avatarUrl} alt="Usuario de la conferencia" />
+        <h1>{firstName} <br /> {lastName}</h1>
       </div>
       <div className="Badge__section-info">
-        <p>Full Stack Web Developer</p>
-        <div>albertofragoso.com</div>
+        <p>{jobTitle}</p>
+        <div>{website}</div>
       </div>
       <div className="Badge__footer">
         #platziconf
--- src/index.js
+++ src/index.js
@@ -8,5 +8,11 @@ import Badge from './components/Badge'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(<Badge />, container)
+ReactDOM.render(<Badge 
+  firstName='Alberto' 
+  lastName='Fragoso'
+  avatarUrl='https://avatars0.githubusercontent.com/u/24305046?s=460&v=4'
+  jobTitle='Full Stack Web Developer'
+  website='albertofragoso.com'
+/>, container)
 
