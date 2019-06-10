b0ed981034aa9e0dad52da82ffbb03e40b753177
albertofragoso <hola@albertofragoso.com>
2019-06-10 18:47:16 -0500

    Agregando Navbar component.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,9 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import './global.css'
 
-import Badge from './components/Badge'
+import BadgeNew from './pages/BadgeNew'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(<Badge 
-  firstName='Alberto' 
-  lastName='Fragoso'
-  avatarUrl='https://avatars0.githubusercontent.com/u/24305046?s=460&v=4'
-  jobTitle='Full Stack Web Developer'
-  website='albertofragoso.com'
-/>, container)
+ReactDOM.render(<BadgeNew />, container)
 
