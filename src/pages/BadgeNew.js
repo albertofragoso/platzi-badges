8ae1b0ece8c8e4a6fbf072d61623a64e2e9d6cf2
albertofragoso <hola@albertofragoso.com>
2019-06-10 19:16:52 -0500

    Creando primera página.

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 
+import './styles/BadgeNew.css'
+import header from '../images/badge-header.svg'
+import Badge from '../components/Badge'
 import Navbar from '../components/Navbar'
 
 class BadgeNew extends Component {
   render() {
-    return <div>
-      <Navbar />
-    </div>
+    return (
+      <>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+        <Badge 
+          firstName="Alberto"
+          lastName="Fragoso"
+          avatarUrl="https://avatars0.githubusercontent.com/u/24305046?s=460&v=4"
+          jobTitle="Full Stack Web Developer"
+          website="albertofragoso.com"
+        />
+      </>
+    )
   }
 }
 
