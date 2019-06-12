f43f0fcf739217915475b352da4a2ba7bddf6149
albertofragoso <hola@albertofragoso.com>
2019-06-11 20:46:39 -0500

    Creando Home page.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Layout from './components/Layout'
+import Home from './pages/Home'
 import Badges from './pages/Badges'
 import BadgeNew from './pages/BadgeNew'
-import NotFound from './pages/NotFound';
+import NotFound from './pages/NotFound'
 
 function App() {
   return(
     <BrowserRouter>
       <Layout>
         <Switch>
+          <Route exact path="/" component={Home} />
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
           <Route component={NotFound}/>
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './styles/Home.css'
+import logo from '../images/platziconf-logo.svg'
+import astronauts from '../images/astronauts.svg'
+
+function Home() {
+  return(
+    <div className="Home">
+      <div className="Home__info">
+        <img src={logo} alt="Logo Conf" />
+        <h3>PRINT YOUR BADGES</h3>
+        <p>... and something else</p>
+        <Link to="/badges" className="btn btn-primary">Entrar</Link>
+      </div>
+      <img src={astronauts} alt="Astronautas Conf" />
+    </div>
+  )
+}
+
+export default Home
