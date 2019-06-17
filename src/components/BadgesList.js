4e8931e9f6dd7b6d9091e50e778fad0c1534234d
albertofragoso <hola@albertofragoso.com>
2019-06-16 20:32:30 -0500

    Agregando SearchBadgesFilter con React Hooks.

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,37 +1,69 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import BadgeListItem from './BadgeListItem'
 
 import './styles/BadgesList.css'
 
-class BadgesList extends Component {
+function useSearchBadges(badges) {
+  const [query, setQuery] = React.useState('')
+  const [filteredBadges, setFilteredBadges] = React.useState(badges)
 
-  render(){
-    const { badges } = this.props
+  React.useMemo(() => {
+    const result = badges.filter(badge => `${badge.firstName} ${badge.lastName}`.toLowerCase().includes(query.toLowerCase()))
+    setFilteredBadges(result) 
+    },[badges, query]
+  )
 
-    if(badges.length === 0) return(
-      <div>
-        <h3>No badges were found.</h3>
-        <Link to="/badges/new" className="btn btn-primary">Create a badge</Link>
+  return { query, setQuery, filteredBadges }
+}
+
+function BadgesList({ badges }) {
+
+  const { query, setQuery, filteredBadges } = useSearchBadges(badges)
+
+  if(filteredBadges.length === 0) return(
+    <>
+      <div className="form-group">
+        <label>Filter Badges</label>
+        <input 
+          type="text" 
+          className="form-control" 
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
       </div>
-    )
+      <h3>No badges were found.</h3>
+      <Link to="/badges/new" className="btn btn-primary">Create a badge</Link>
+    </>
+  )
 
-    return(
+  return(
+    <>
+      <div className="form-group">
+        <label>Filter Badges</label>
+        <input 
+          type="text" 
+          className="form-control" 
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
+      </div>
+
       <ul className="list-unstyled">
         {
-          badges.map(badge => {
+          filteredBadges.map(badge => {
             return (
               <li key={badge.id} className="BadgesListItem">
-                <Link className="text-reset text-decoration-none"  to={`/badges/${badges.id}`}>
+                <Link className="text-reset text-decoration-none" to={`/badges/${badges.id}`}>
                   <BadgeListItem badge={badge} />
                 </Link>
               </li>
             )
           })
         }
-      </ul> 
-    )
-  }
+      </ul>
+    </> 
+  )
 }
 
 export default BadgesList
